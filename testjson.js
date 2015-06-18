acb0f31af1d2b7b36eacc83638ffe5a060048cbe
lovechopra1 <lovechopra1@gmail.com>
2015-06-18 19:07:52 +0530

    Create testjson.js

---
+++ testjson.js
@@ -0,0 +1,17 @@
+var mcg_page_data = {"items": [{
+        value: "cooking",
+        label: "Cooking",
+        id: "1"
+    },
+    {
+        value: "C++",
+        label: "C++",
+        id: "2"
+    },
+    {
+        value: "craftsmanship",
+        label: "Software Craftsmanship",
+        id: "3"
+    }
+    ]
+    }
